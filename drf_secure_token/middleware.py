c16bd4d7b4e7e100faa3c3c6be3ff289e15eae82
tima <tima.akulich@gmail.com>
2015-12-19 11:22:52 +0300

    Fixed middleware

--- drf_secure_token/middleware.py
+++ drf_secure_token/middleware.py
@@ -4,7 +4,7 @@ from drf_secure_token.models import Token
 
 class UpdateTokenMiddleware(object):
     def process_response(self, request, response):
-        auth_token = request.META.get('HTTP_AUTHORIAZATION', ' ').split(' ')
+        auth_token = request.META.get('HTTP_AUTHORIZATION', ' ').split(' ')
         if request.user.is_authenticated() and auth_token[0].lower() == 'token':
             token = Token.objects.filter(key=auth_token[1]).first()
             if token:

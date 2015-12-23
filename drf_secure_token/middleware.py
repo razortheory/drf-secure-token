c23b4e1e180095d01d8d6a856209d44834838f2c
tima <tima.akulich@gmail.com>
2015-12-23 12:29:31 +0300

    Update UpdateToken middleware

--- drf_secure_token/middleware.py
+++ drf_secure_token/middleware.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from drf_secure_token.models import Token
+
+UPDATE_TOKEN = getattr(settings, 'UPDATE_TOKEN', True)
 
 
 class UpdateTokenMiddleware(object):
     def process_response(self, request, response):
         auth_token = request.META.get('HTTP_AUTHORIZATION', ' ').split(' ')
-        if hasattr(request, 'user') and request.user.is_authenticated() and auth_token[0].lower() == 'token':
+        if hasattr(request, 'user') and request.user.is_authenticated() and auth_token[0].lower() == 'token' and UPDATE_TOKEN:
             token = Token.objects.filter(key=auth_token[1])
             if token:
                 new_token = Token.generate_key()

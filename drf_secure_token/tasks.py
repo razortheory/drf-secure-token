bc147afed62a0936a3dfcba49bd4c5448f189b96
Maxim Lomakin <maxim@razortheory.com>
2016-08-25 18:58:33 +0300

    Make celery to delete old tokens switchable.

--- drf_secure_token/tasks.py
+++ drf_secure_token/tasks.py
@@ -4,13 +4,15 @@ from celery.task import periodic_task
 from celery.schedules import crontab
 
 from drf_secure_token.models import Token
+from drf_secure_token.settings import REMOVE_TOKENS_THROUGH_CELERY
 
 
-@periodic_task(run_every=crontab(minute=0))
-def delete_old_tokens():
-    now = timezone.now()
+if REMOVE_TOKENS_THROUGH_CELERY:
+    @periodic_task(run_every=crontab(minute=0))
+    def delete_old_tokens():
+        now = timezone.now()
 
-    qs = Token.objects.all()
-    qs = qs.filter(dead_in__lt=now)
+        qs = Token.objects.all()
+        qs = qs.filter(dead_in__lt=now)
 
-    qs.delete()
+        qs.delete()

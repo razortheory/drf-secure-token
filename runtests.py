3bff35e2985fb394bb3a96cec8998fa1138999b6
Vlad Samuta <vlad.s@razortheory.com>
2019-09-20 14:41:16 +0300

    Changed test run code

--- runtests.py
+++ runtests.py
@@ -2,17 +2,9 @@
 import os
 import sys
 
-import django
-from django.conf import settings
-from django.test.utils import get_runner
-
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
-    django.setup()
 
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+    from django.core.management import execute_from_command_line
 
-    failures = test_runner.run_tests(['tests'])
-
-    sys.exit(bool(failures))
+    execute_from_command_line(sys.argv[:1] + ['test'] + sys.argv[1:])
